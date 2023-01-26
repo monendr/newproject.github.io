2bc098a11091e0a4781a797227dc56feee98d9bc
Monendra Ningthoujam <monendr@users.noreply.github.com>
2023-01-26 12:46:12 +0530

    ok

--- new.js
+++ new.js
@@ -1,47 +1,45 @@
-function submitForm(action) {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    if (action == "login") {
-        // Send a login request to the server
-        // ...
-    } else if (action == "signup") {
-        var email = document.getElementById("email").value;
-        // Send a signup request to the server
-        // ...
-    }
-}
-
-function showSignupForm() {
-    document.getElementById("login-form").style.display = "none";
-    document.getElementById("signup-form").style.display = "block";
-}
-
-function showLoginForm() {
-    document.getElementById("login-form").style.display = "block";
-    document.getElementById("signup-form").style.display = "none";
-}
-$(".dropdown").hover(
-    function() {
-        $('.dropdown-content', this).stop( true, true ).slideDown("fast");
-    },
-    function() {
-        $('.dropdown-content', this).stop( true, true ).slideUp("fast");
-    }
-);
-  function showMore(service){
-    if(service == 1){
-      alert('Learn More about Service 1');
-    }
-    else if(service == 2){
-      alert('Learn More about Service 2');
-    }
-    else if(service == 3){
-      alert('Learn More about Service 3');
-    }
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
   }
 
- function showContactModal(memberId) {
-    // code to show a contact modal for the selected team member goes here
-    console.log("Showing contact modal for member with ID: " + memberId);
-  }
-  
+}
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
+}
